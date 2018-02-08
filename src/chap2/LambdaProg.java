b2c1d70c21ca39dde4309e61649cb48d4cfb9ca3
novadhaliwal <boparainova@gmail.com>
2018-02-08 15:10:34 -0500

    Various ways to implement lambda

--- src/chap2/LambdaProg.java
+++ src/chap2/LambdaProg.java
@@ -7,25 +7,35 @@ public class LambdaProg {
 		public void lambda()
 		{
 			 int num=3;
-			 
 			 Runnable r= new Runnable() {
+				 
+		//1. using anonymous class
 				  int num=4;
 				  public void run() {
 					  int num=5;
-					  System.out.println(num);
-				  }
-				 
+					  System.out.println(this.num);
+				  } 
 			 };
-			 
-			 
 			 r.run();
-		}
-
+		
+		
+	/*	//2. direct lambda
+		 Runnable d= ()->System.out.println("hello");
+		 
+		 //3. lambda passed directly
+		 
+		 public static void process(Runnable t) {
+			 t.run();
+		 } 
+		 */
+		} 
 		public static void main(String[] args) {
 			
 			LambdaProg l= new LambdaProg();
 			l.lambda();
 			
 		}
+		
+		
 
 }
